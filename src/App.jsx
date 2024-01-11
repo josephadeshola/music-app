7a9c9138d1173fad5f00d61e67c7626f66ae79ba
Adeshola Ayomide <josephay125d@gmail.com>
2024-01-11 10:05:17 -0800

    code update

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,5 @@
-import { useState } from 'react'
 import './App.css'
-import Home from './components/Home'
 import Navbar from './components/Navbar'
-import MusicApi from './components/musicApi'
 
 function App() {
   return (
